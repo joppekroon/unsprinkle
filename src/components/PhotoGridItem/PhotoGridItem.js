77f9d6c521ef0270f79e33fefa1b8189df3e9c1c
Joppe Kroon <J.Kroon@topdesk.com>
2021-06-22 17:03:50 +0200

    Ex4: Tag overflow

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -44,17 +44,25 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 `;
 
 const Tag = styled.li`
+  display: inline;
+
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+  
+  line-height: calc(0.875rem + 8px);
+  
+  & + & {
+    margin-left: 8px;
+  }
 `;
 
 export default PhotoGridItem;
